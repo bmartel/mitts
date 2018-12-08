9a65546b90bc7f747229440f2fe134976cdf5339
Brandon Martel <brandonmartel@gmail.com>
2018-12-08 10:51:56 -0600

    parse the query string and merge into params

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 import m from "mithril";
 import Router from "find-my-way";
+import url from "url";
 
 const defaultRoute = (req, res) => {
   res.statusCode = 404;
@@ -21,14 +22,15 @@ export default (loader, handler, routes, options = defaultOptions) => {
       const component = routes[route];
       const onmatch = component.onmatch || (() => component);
       const render = component.render || (a => a);
+      const query = url.parse(req.url, { parseQueryString: true }).query;
+      const attrs = Object.assign({}, params, { query });
 
-      const attrs = Object.assign({}, params, req.query);
-
+      console.log(query.things);
       Promise.resolve()
         .then(() => m(onmatch(attrs, req.url) || "div", attrs))
         .then(render)
         .then(handler)
-        .then(html => html(loader, req, res, params))
+        .then(html => html(loader, req, res, attrs))
         .catch(err => {
           console.error(err);
           options.defaultRoute(req, res);
